c67bcec0e5e4c69683459418b4dcb0e2f2d43bc2
noomdalv <vladlunasan@gmail.com>
2020-04-13 18:36:08 -0500

    created checkLoginStatus method in App.js to check for user session

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 
@@ -8,9 +9,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       loggedInStatus: "NOT_LOGGED_IN",
-      user: {
-
-      }
+      user: {}
     }
     this.handleLogin = this.handleLogin.bind(this);
   }
@@ -20,6 +19,30 @@ class App extends React.Component {
       loggedInStatus: "LOGGED_IN",
       user: data.user
     })
+  }
+
+  checkLoginStatus() {
+    axios.get("http://localhost:3001/logged_in", { withCredentials:true })
+    .then(response => {
+      if (response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "LOGGED_IN",
+          user: response.data.user
+        })
+      } else if (!response.data.logged_in && this.state.loggedInStatus === "LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN",
+          user: {}
+        })
+      }      
+    })
+    .catch(error => {
+      console.log("logged in error =>", error)
+    })
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
   }
 
   render() {
